2179452753d4925cf9128092a86bdb55c8989480
Sakal05 <samnang.sakal@gmail.com>
2024-04-05 23:13:07 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,295 @@
-import Image from "next/image";
+"use client";
+
+import { ethers } from "ethers";
+import ToDoContractABI from "../contract/ToDoContractABI.json";
+import { useState, useEffect } from "react";
+// import BigNumber from "bignumber.js";
 
 export default function Home() {
+  const [newTaskName, setNewTaskName] = useState("");
+  const [shareAddresses, setShareAddresses] = useState([]);
+  const [showCreateTaskModal, setShowCreateTaskModal] = useState(false);
+  const [address, updateAddress] = useState("");
+  const [balance, updateBalance] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [connected, setConnected] = useState(false);
+  const [sharedTasks, setSharedTasks] = useState([]);
+  const [taskCreatorAddress, setTaskCreatorAddress] = useState("");
+
+  let signer = null;
+
+  let provider;
+
+  if (window.ethereum == undefined) {
+    console.log("MetaMask not installed; using read-only defaults");
+    provider = ethers.getDefaultProvider();
+  } else {
+    window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum));
+    // provider = new ethers.providers.Web3Provider(window.ethereum);
+    signer = provider.getSigner();
+  }
+
+  const initContract = () => {
+    const todoContract = new ethers.Contract(
+      "0x794Bd9510A8C987946077089C04EF56b954cb098",
+      ToDoContractABI,
+      provider
+    ).connect(signer);
+    return todoContract;
+  };
+
+  async function connectWallet() {
+    console.log("Wallet connect status: ", connected);
+    if (!connected) {
+      if (window.ethereum == null) {
+        console.log("MetaMask not installed; using read-only defaults");
+        provider = ethers.getDefaultProvider();
+      } else {
+        provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+        await provider.send("eth_requestAccounts", []);
+        signer = provider.getSigner();
+      }
+      console.log("here connecting");
+
+      setConnected(true);
+      getUserAddress();
+      getWalletBalance();
+      getMyTasks();
+    }
+  }
+
+  async function getUserAddress() {
+    const address = await signer.getAddress();
+    updateAddress(address);
+    return address;
+  }
+
+  // Define a function to fetch the balance of the signer's wallet address
+  const getWalletBalance = async () => {
+    try {
+      const balance = await signer.getBalance();
+      updateBalance(ethers.utils.formatEther(balance).toString());
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const getMyTasks = async () => {
+    try {
+      const contract = initContract();
+      const tasks = await contract.getMyTasks();
+      console.log(tasks);
+      setTasks(tasks);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getSharedTasks = async () => {
+    try {
+      let contract = initContract();
+      const tasks = await contract.getSharedTasks(taskCreatorAddress);
+      console.log(tasks);
+      setSharedTasks(tasks);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onCreateNewTask = async () => {
+    try {
+      let contract = initContract();
+      console.log(shareAddresses)
+      // console.log(contract);
+      // const taskName = "Bors Teas"; // Set your task name here
+      // const sharedWith = ["0x5852231D8a00306A67DfB128AEd50c1573411d60"]; // Set addresses to share the task with here
+      // contract.connect(signer);
+      const tasks = await contract.createTask(newTaskName, shareAddresses);
+      console.log(tasks);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const markOwnTaskAsDone = async (id) => {
+    try {
+      let contract = initContract();
+      const markTaskDone = await contract.markAsCompleted(address, id);
+      console.log(markTaskDone);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const markSharedTaskAsDone = async (id) => {
+    try {
+      let contract = initContract();
+      const markTaskDone = await contract.markAsCompleted(
+        taskCreatorAddress,
+        id
+      );
+      console.log(markTaskDone);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (signer) {
+      getUserAddress();
+      getWalletBalance();
+      getMyTasks();
+      setConnected(true)
+    } else {
+      connectWallet()
+      setConnected(true)
+    }
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center justify-center min-h-screen p-8">
+      <nav className="flex justify-between w-full mb-8">
+        <div>My Address: {address}</div>
+        <div>Balance: {balance}</div>
+        <button className="btn" onClick={connectWallet}>
+          {connected ? "Connected" : "Connect Wallet"}
+        </button>
+      </nav>
+      <div className="w-full">
+        <table className="table-auto w-full">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">ID</th>
+              <th className="px-4 py-2">Task Name</th>
+              <th className="px-4 py-2">Status</th>
+              <th className="px-4 py-2">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task, index) => (
+              <tr key={index}>
+                <td className="border px-4 py-2">{ethers.BigNumber.from(task.id).toNumber()}</td>
+                <td className="border px-4 py-2">{task.taskName}</td>
+                <td className="border px-4 py-2">
+                  {task.completed ? "Completed" : "Not Yet"}
+                </td>
+                <td className="border px-4 py-2">
+                  {!task.completed ? (
+                    <button
+                      className="btn"
+                      onClick={() => {
+                        markOwnTaskAsDone(ethers.BigNumber.from(task.id).toNumber());
+                      }}
+                    >
+                      Mark As Complete
+                    </button>
+                  ) : (
+                    <button className="btn" disabled>
+                      Completed
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="mt-8">
+        <button className="btn" onClick={() => setShowCreateTaskModal(true)}>
+          Create New Task
+        </button>
+      </div>
+      {showCreateTaskModal && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
+          <div className="bg-white p-8 rounded-md">
+            <h2 className="text-lg font-semibold mb-2">Create New Task</h2>
+            <form onSubmit={onCreateNewTask} className="flex flex-col">
+              <input
+                type="text"
+                placeholder="Task Name"
+                value={newTaskName}
+                onChange={(e) => setNewTaskName(e.target.value)}
+                className="input mb-2"
+              />
+              <input
+                type="text"
+                placeholder="List of Share Addresses (comma separated)"
+                value={shareAddresses.join(",")}
+                onChange={(e) =>
+                  setShareAddresses(
+                    e.target.value.split(",").map((address) => address.trim())
+                  )
+                }
+                className="input mb-2"
+              />
+              <div className="flex justify-end">
+                <button
+                  type="button"
+                  className="btn mr-2"
+                  onClick={() => setShowCreateTaskModal(false)}
+                >
+                  Cancel
+                </button>
+                <button type="submit" className="btn">
+                  Create Task
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      )}
+      <div className="w-full">
+        <h2 className="text-lg font-semibold mb-2">Shared Tasks</h2>
+        <div className="w-full">
+          <input
+            type="text"
+            placeholder="Task Creator's Address"
+            value={taskCreatorAddress}
+            onChange={(e) => setTaskCreatorAddress(e.target.value)}
+            className="input mb-2"
+          />
+          <button className="btn" onClick={getSharedTasks}>
+            Get Shared Tasks
+          </button>
+        </div>
+        <table className="table-auto w-full mt-4">
+          <thead>
+            <tr>
+              <tr>
+                <th className="px-4 py-2">ID</th>
+                <th className="px-4 py-2">Task Name</th>
+                <th className="px-4 py-2">Status</th>
+                <th className="px-4 py-2">Actions</th>
+              </tr>
+            </tr>
+          </thead>
+          <tbody>
+            {sharedTasks.map((task, index) => (
+              <tr key={index}>
+                <td className="border px-4 py-2">{index}</td>
+                <td className="border px-4 py-2">{task.taskName}</td>
+                <td className="border px-4 py-2">
+                  {task.completed ? "Completed" : "Not Yet"}
+                </td>
+                <td className="border px-4 py-2">
+                  {!task.completed ? (
+                    <button
+                      className="btn"
+                      onClick={() => {
+                        markSharedTaskAsDone(ethers.BigNumber.from(task.id).toNumber());
+                      }}
+                    >
+                      Mark As Complete
+                    </button>
+                  ) : (
+                    <button className="btn" disabled>
+                      Completed
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </main>
   );
